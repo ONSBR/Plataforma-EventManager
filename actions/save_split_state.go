b5dbf704c0c70d700dd0291fed473b49d13b1bd7
Leitao <uncodead@gmail.com>
2020-02-18 16:56:12 -0300

    reprocess

--- actions/save_split_state.go
+++ actions/save_split_state.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/ONSBR/Plataforma-EventManager/domain"
 	"github.com/ONSBR/Plataforma-EventManager/infra"
-	"github.com/ONSBR/Plataforma-EventManager/sdk"
 	log "github.com/sirupsen/logrus"
 )
 
